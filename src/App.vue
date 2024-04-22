f874b163d04c922a38685bdb43c9f18a9c96ab94
Francesco-Rianna <maradonino98@hotmail.it>
2024-04-22 16:54:00 +0200

    feat : created the data structure for the first newly created component 'appHeader'

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,42 @@
 <script>
+import AppHeader from './components/AppHeader.vue'
+  export default {
+    components : {
+      AppHeader
 
+    },
+    data() {
+    return {
+        contacts: [
+            {
+                'number': "1.800.654.125.452",
+                'email': "Contact Us",
+                "alumn": "Alumni",
+                "campus": "Campus"
+            },
+            
+            ],
+
+        social: [
+          {
+            'fb' : '',
+            'twitter': '',
+            'yt' :'',
+            'insta' :''
+
+          }
+        ]
+        }         
+    }
+}
+  
 </script>
 
 <template>
+  <AppHeader> </AppHeader>
 </template>
 
-<style scoped>
+<style lang="scss">
+ @use './style/generic'
 
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name : 'AppHeader'
+}
+</script>
+
+<template>
+    <div class="ms-container">
+        
+    </div>
+
+</template>
+
+<style scoped lang="scss">
+</style>
