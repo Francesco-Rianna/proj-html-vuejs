b45f2c27b9dc39fe040284f51a4ad9129b843c46
Francesco-Rianna <maradonino98@hotmail.it>
2024-04-24 16:26:56 +0200

    feat : created a new component footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,19 @@
+<script>
+export default{
+    name: 'AppFooter'
+}
+</script>
+
+<template>
+    <div class="ms-container-large">
+        <h1>ciao</h1>
+    </div>
+
+</template>
+
+<style scoped lang="scss">
+.ms-container-large{
+    width: 95%;
+    margin: 0 auto;
+}
+</style>
